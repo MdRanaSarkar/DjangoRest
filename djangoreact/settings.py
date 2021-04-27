483f45d118587f2f3df7187427d98c2717150433
unknown <ranasarkar1503031@gmail.com>
2021-04-28 00:52:56 +0600

    final

--- djangoreact/settings.py
+++ djangoreact/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
 ]
-CORS_ALLOWED_ORIGINS = True
+CORS_ORIGIN_ALLOW_ALL = True
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
