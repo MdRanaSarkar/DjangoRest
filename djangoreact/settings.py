ddd6407de62f8cc9d5c3a2090fe111aed153e7f4
unknown <ranasarkar1503031@gmail.com>
2021-04-28 00:43:58 +0600

    cors error solved

--- djangoreact/settings.py
+++ djangoreact/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3ziqu-uk2*zps@v0$e)o0up-@8so3uk%_f#kkpzrzsif*(bml*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -39,9 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'djangoreactapp',
     'rest_framework',
+    'corsheaders',
 ]
+CORS_ALLOWED_ORIGINS = True
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
